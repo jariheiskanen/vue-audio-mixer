f583bb31e8ed29c7255bd520f47cd67c69c80a25
jariheiskanen <jari.heiskanen.koulu@gmail.com>
2026-02-07 16:06:34 +0200

    Initial layout and functionality
    Removed template and start actual audio mixer layout with functionality

--- src/App.vue
+++ src/App.vue
@@ -2,52 +2,37 @@
 npm run dev
 npm run build
 npm run deploy
--->>
+-->
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import AudioChannel from './components/AudioChannel.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="layout-wrapper">
+    <div class="main-section">
+      <!--main section here-->
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="channel-wrapper">
+      <AudioChannel />
+      <AudioChannel />
+      <AudioChannel />
+      <AudioChannel />
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+
+.main-section
+{
+  min-height: 545px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.channel-wrapper
+{
+  margin-top: auto;
+  display: flex;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/AudioChannel.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref } from 'vue';
+
+const AudioRef = ref(null);
+
+function handleFile(e) 
+{
+    const file = e.target.files[0];
+    const url = URL.createObjectURL(file);
+
+    //load selected file into audio element
+    AudioRef.value.src = url;
+    AudioRef.value.load();
+
+    //analyze audio with Web Audio API
+    //visualize on canvas
+}
+
+</script>
+
+<template>
+    <div class="channel">
+        <label class="add-audio-label"><div class="circle">+</div>
+            <input type="file" accept="audio/*" @change="handleFile" hidden />
+        </label>
+
+        <audio ref="AudioRef" class="audio" hidden></audio>
+        <canvas class="canvas"></canvas>
+    </div>
+</template>
+
+<style scoped>
+
+.channel
+{
+    position: relative;
+    border: 1px solid #0da90d;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    background-color: #cbcbcb;
+}
+
+.add-audio-label
+{
+    font-size: 30px;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+.circle
+{
+    width: 40px;
+    height: 40px;
+    border: 2px solid #0da90d;
+    border-radius: 30px;
+    text-align: center;
+    line-height: 40px;
+}
+
+canvas
+{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+</style>
